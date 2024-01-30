883797f3bd3c85539a7188bb542813069be41346
Amrita Gupta <amamrita0818@gmail.com>
2024-01-31 02:14:40 +0530

    complete story.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Hero from '../Hero'
+import Story from './Story';
+import Pledge from './Pledge';
 
 const description1 = `Experience Ocean Freshness,`;
 const description2 = `Handpicked by Noulamer`;
-const button = {title: `Explore Now` , url: `#`};
+const button = {title: `Explore Now`, url: `#`};
 const heroImage = `/hero.png`
 const Home = () => {
   return (
     <div className='w-full'>
       <Hero description1={description1} description2={description2} button={button} heroImage={heroImage}/>
-
+      <Story/>
+      <Pledge/>
     </div>
   )
 }
---
+++ src/components/Home/Story.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import box from '../../assets/box.png';
+import home1 from '../../assets/home1.jpg'
+import { Link } from 'react-router-dom';
+import { GoArrowRight } from "react-icons/go";
+
+const Story = (button) => {
+    return (
+        <div className='w-full'>
+            <div className='w-[80%]  max-w-[1300px] py-8 my-20 mx-auto'>
+                <div className='flex gap-44'>
+                    <div className='relative w-[50%]'>
+                        <div className='absolute left-[-70px]'>
+                            <img src={box} alt="" width={210} height={190} className='object-cover' />
+                        </div>
+                        <div className='absolute top-16  rounded-tl-[48px] '>
+                            <img src={home1} alt="" width={520} height={830} className=' object-cover border rounded-tl-3xl' />
+                        </div>
+                    </div>
+                    <div className='w-[50%] mt-40'>
+                        <div className='flex gap-4 items-center '>
+                            <div className='w-4 h-4 bg-[#EF4030] '></div>
+                            <h4 className='text-xl font-[600] text-[#0A385A]'>Mauritius&apos; Finest Sea Harvest</h4>
+                        </div>
+                        <h1 className='py-4 text-5xl font-[600] text-[#0A385A]'>Sea to Table Story</h1>
+                        <div className='flex flex-col gap-y-4'>
+                            <p className='text-[19px] font-[400] text-[#1E1E1E]'>
+                                Choosing the right source for your seafood is crucial for taste and quality. At Noulamer, we're not just a business; we&apos;re a tradition, deeply rooted in Mauritius since 1974. Our selection ranges across the freshest fish, shrimp, and more, all locally sourced.
+                            </p>
+                            <p className='text-[19px] font-[400] text-[#1E1E1E]'>We&apos;ve nurtured strong relationships with local fishermen along the South Coast, ensuring that every product in our stall and in major retailers reflects our commitment to excellence. </p>
+                            <p className='text-[19px] font-[400] text-[#1E1E1E]'>Dive into our story and discover why every bite is a testament to our heritage and dedication.</p>
+                        </div>
+                        <Link className='text-[#FFFFFF] bg-[#0A385A] flex items-center gap-3 mt-8 px-8 w-[42.5%] py-5 rounded-full font-[600] text-[17px]'>Read About Us <GoArrowRight className='w-6 h-6' />
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Story
