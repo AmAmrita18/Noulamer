216b55863671fd1529979cffc99584ccd92b9de8
Amrita Gupta <amamrita0818@gmail.com>
2024-01-30 14:11:54 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Layout from './Layout'
+
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+      </Route>
+    </Routes>
+  </Router>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Root />
+)
